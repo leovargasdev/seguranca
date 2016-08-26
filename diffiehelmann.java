54656640c5b906771ab45a4743fbad909cee3aa5
LeeonardoVargas <leu1607@hotmail.com>
2016-08-26 16:13:10 -0300

    arrumando codigo

--- diffiehelmann.java
+++ diffiehelmann.java
@@ -1,13 +1,12 @@
-//
 import java.util.Scanner;
 class diffiehelmann{
     public static void main(String [] agrs){
         Scanner scanner = new Scanner(System.in);
-        double m = 23;                                  // modulo
-        double base = 5;                                //  base
-        double chave = scanner.nextDouble();            // chave secreta
-        System.out.println(Math.pow(base,chave) % m);   // chave intermediaria
-        double ci = scanner.nextDouble();               // lendo a chave intermediaria da outra pessoa
-        System.out.println("msg:" + (Math.pow(ci,chave) % m));
+        double m = 23;                           // modulo
+        double b = 5;                            // base
+        double c = scanner.nextDouble();         // lenado chave secreta
+        System.out.println(Math.pow(b,c) % m);   // gerando chave intermediaria
+        double ci = scanner.nextDouble();        // lendo a chave intermediaria da outra pessoa
+        System.out.println("msg:" + (Math.pow(ci,c) % m));
     }
 }
