e7b68c7981c7cfc36f5f778ad37ee4c1df8bc099
LeeonardoVargas <leu1607@hotmail.com>
2016-09-18 16:24:27 -0300

    adicionando o trabalho do buffer overflow

--- Diffie Hellman/diffiehellman.java
+++ Diffie Hellman/diffiehellman.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-class diffiehelmann{
+class diffiehellman{
     public static void main(String [] agrs){
         Scanner scanner = new Scanner(System.in);
         double m = 23;                           // modulo
         double b = 5;                            // base
-        double c = scanner.nextDouble();         // lenado chave secreta
+        double c = scanner.nextDouble();         // lendo chave secreta
         System.out.println(Math.pow(b,c) % m);   // gerando chave intermediaria
         double ci = scanner.nextDouble();        // lendo a chave intermediaria da outra pessoa
         System.out.println("msg:" + (Math.pow(ci,c) % m));
