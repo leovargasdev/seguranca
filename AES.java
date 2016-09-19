dd54fa145c4563117e65db1f79a95bdca88e919f
LeeonardoVargas <leu1607@hotmail.com>
2016-09-19 11:49:52 -0300

    adicionando o AES

---
+++ AES.java
@@ -0,0 +1,29 @@
+import javax.crypto.spec.*;
+import javax.crypto.*;
+
+public class AES{
+	static String IV = "AAAAAAAAAAAAAAAA";
+	static String textopuro = "teste texto 12345678\0\0\0";
+	static String chaveencriptacao = "0123456789abcdef";
+	public static void main(String [] args) throws Exception{
+		System.out.println("Texto Puro: " + textopuro);
+		byte[] textoencriptado = encrypt(textopuro, chaveencriptacao);
+		System.out.println("Texto Encriptado: ");
+		for (int i = 0; i < textoencriptado.length; i++)
+			 System.out.print(new Integer(textoencriptado[i])+" ");
+		String textodecriptado = decrypt(textoencriptado, chaveencriptacao);
+		System.out.println("\nTexto Decriptado: " + textodecriptado);
+	}
+	public static byte[] encrypt(String textopuro, String chaveencriptacao) throws Exception {
+		Cipher encripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+		encripta.init(Cipher.ENCRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+		return encripta.doFinal(textopuro.getBytes("UTF-8"));
+	}
+	public static String decrypt(byte[] textoencriptado, String chaveencriptacao) throws Exception{
+		Cipher decripta = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(chaveencriptacao.getBytes("UTF-8"), "AES");
+		decripta.init(Cipher.DECRYPT_MODE, key,new IvParameterSpec(IV.getBytes("UTF-8")));
+		return new String(decripta.doFinal(textoencriptado),"UTF-8");
+	}
+}
