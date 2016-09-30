19983ee7b04e77372c6bd3c6b806de4c24fa099c
LeeonardoVargas <leu1607@hotmail.com>
2016-09-29 22:34:03 -0300

    arrumando cifra da substituição

--- Criptografia Classica/substituicao/substituicao.java
+++ Criptografia Classica/substituicao/substituicao.java
@@ -3,27 +3,37 @@ import java.io.*;
 import java.nio.file.*;
 public class substituicao{
     public byte[] jurema;
-    public byte[] key;
+    public byte[] key, ascii = new byte[256];
     public substituicao(byte[] v) throws IOException{
         this.jurema = v;
         this.key = Files.readAllBytes(Paths.get("keys/key1"));
+        for(int a = 0; a < ascii.length; a++)
+            this.ascii[a] = (byte)a;
     }
     public void criptografar() throws IOException{
         FileOutputStream w = new FileOutputStream(new File("substituicao/outputs/out_crip.txt"));
-        /*int[] a = {1,2,3,4,5}, b = {2,4,1,5,3};
-        for(int y = 0; y < a.length; y++)
-            for(int t = 0; t < b.length; t++)
-                if(a[y] == b[t])
-                    a[y] =
-        for(int u = 0; u < this.jurema.length; u ++)
-            for(int e = 0; e < this.key.length; e++)
-                if((char)this.jurema[u] == (char)e)
-                    this.jurema[u] = key[e];
-                    //System.out.println("vetor ju: " + (char)this.jurema[u] + ", vetor e: " + (char)key[e]);
-                    //System.out.println("vetor ju: " + (char)this.jurema[u] + ", vetor e: " + (char)key[e]);
+        for(int u = 0; u < this.jurema.length; u++)
+            for(int e = 0; e < this.ascii.length; e++)
+                if(this.jurema[u] == this.ascii[e]){
+                    this.jurema[u] = this.key[e];
+                    break;
+                }
         w.write(this.jurema);
-        w.close();*/
-        //System.out.println(this.jurema[0] + ", valor em char: " + (char)this.jurema[0]);
-        //System.out.println(key[(int)this.jurema[0]] + ", valor em char: " + (char)key[(int)this.jurema[0]]);
+        w.close();
     }
+    /*public static void main(String [] args){
+        int[] ordenado = {1,2,3,4,5};
+        int[] texto = {3,1,5};
+        int[] desordenado = {2,4,5,3,1};
+        for(int k = 0; k < texto.length; k++)
+            for(int t = 0; t < ordenado.length; t++)
+                if(texto[k] == ordenado[t]){
+                    texto[k] = desordenado[t];
+                    break;
+                }
+
+        for(int k = 0; k < texto.length; k++)
+            System.out.print(texto[k] + "-");
+        System.out.println();
+    }*/
 }
